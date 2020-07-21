b403c8803f655e50477e9198153794969b70db87
poznyi11 <poznyi11@gmail.com>
2020-07-21 21:31:34 +0300

    ADD commit task_1

---
+++ src/task_1/MainKotlin.java
@@ -0,0 +1,15 @@
+package task_1;
+
+import task_1.task_1_1.MaxLengthEven;
+
+public class MainKotlin {
+    public static void main(String[] args) {
+//        MyKotlin my = new MyKotlin();
+//        System.out.println(my.getAge());
+//        my.maxLengthEven();
+//        System.out.println(my.createList());
+
+        MaxLengthEven maxLengthEven = new MaxLengthEven();
+        maxLengthEven.maxLengthEven();
+    }
+}
---
+++ src/task_1/task_1_2/com/company/professions/Person.kt
@@ -0,0 +1,6 @@
+package task_1.task_1_2.com.company.professions
+
+open class Person {
+    open lateinit var fullName: String
+    open var age: Int = 0
+}
